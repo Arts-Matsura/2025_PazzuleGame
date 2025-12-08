68036d5fadb266c39ffd4b61c87b83fb277579d4
Arts-Matsura <k235025@kccollege.ac.jp>
2025-12-08 10:32:37 +0900

    memo

--- project/Source/titleScene.h
+++ project/Source/titleScene.h
@@ -13,21 +13,5 @@ public:
 	void Update() override;
 	void Draw() override;
 private:
-	int cursor;
-	int repeatCount;
-	int GetCursorKey();
-	void menu();
 
-	enum Mode {
-		M_LOGO,
-		M_MENU,
-		M_DEMO,
-		M_MAX
-	};
-	Mode mode;
-	float timer;
-
-	int hBack;
-	int hLogo;
-	int hDemo;
 };
