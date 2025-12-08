9e02136aa3351760d4749a901537e156ddc90ddb
setoaiki <k235013@kccollege.ac.jp>
2025-12-08 12:08:46 +0900

    今日の成果

--- project/Source/TIle.cpp
+++ project/Source/TIle.cpp
@@ -4,6 +4,20 @@ Tile::Tile()
 {
 }
 
+Tile::Tile(int _id)
+{
+	id = _id;
+}
+
 Tile::~Tile()
 {
+}
+
+void Tile::setTile(Vector2I _delta)
+{
+}
+
+void Draw()
+{
+	
 }
--- project/Source/TIle.h
+++ project/Source/TIle.h
@@ -1,11 +1,18 @@
 #pragma once
+#include"Transform.h"
+#include"../Library/gameObject.h"
 
 class Tile
 {
 public:
 	Tile();
+	Tile(int _id);
 	~Tile();
 
 	void Draw();
+	void setTile(Vector2I _delta);
 	int id;
+	Vector2I delta;
+	Transform transform;
+
 };
