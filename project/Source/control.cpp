71e0d5090ee6711821edc88a26f0611468317548
k235016-tamada <k235016@kccollege.ac.jp>
2025-12-15 09:23:55 +0900

    コントローラー　事項追加

--- project/Source/control.cpp
+++ project/Source/control.cpp
@@ -1,17 +1,23 @@
-#include "control.h"
+#include "Control.h"
 
-Control::Control()
-{
-}
-
-Control::~Control()
-{
-}
+Control::Control() {}
+Control::~Control() {}
 
 void Control::Update()
 {
+    h = 0;
+    v = 0;
+
+    // ç∂âE
+    if (CheckHitKey(KEY_INPUT_A)) h = -1;
+    if (CheckHitKey(KEY_INPUT_D)) h = +1;
+
+    // è„â∫
+    if (CheckHitKey(KEY_INPUT_W)) v = -1;
+    if (CheckHitKey(KEY_INPUT_S)) v = +1;
 }
 
 void Control::Draw()
 {
+    // ï`âÊÇÕïsóv
 }
--- project/Source/control.h
+++ project/Source/control.h
@@ -1,13 +1,21 @@
 #pragma once
-#include"../Library/gameObject.h"
+#include "../Library/gameObject.h"
+
+// WASD 入力だけを管理するクラス
 class Control : public GameObject
 {
 public:
-	Control();
-	~Control();
-	void Update()override;
-	void Draw()override;
+    Control();
+    ~Control();
+
+    void Update() override;
+    void Draw() override;
+
+    // 取得（-1 / 0 / +1）
+    int GetHorizontal() const { return h; }  // A=-1 / D=+1
+    int GetVertical()   const { return v; }  // W=-1 / S=+1
+
 private:
-
-
+    int h = 0;  // 左右入力
+    int v = 0;  // 上下入力
 };
